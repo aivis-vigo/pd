90007366b813ad78b32dff74db8e842460a019f2
aivis-vigo <aivisvigoreimarts@gmail.com>
2023-04-18 20:55:30 +0300

    add

--- main.php
+++ main.php
@@ -1,3 +1,16 @@
 <?php declare(strict_types=1);
 
-echo "1";
+require_once "vendor/autoload.php";
+
+$url = "https://www.latvijasbanka.lv/vk/ecb.xml";
+use Thelia\CurrencyConverter;
+
+
+$xml = simplexml_load_file($url);
+$json = json_encode($xml);
+$response = json_decode($json, true);
+$allCurrencies = $response["Currencies"]["Currency"];
+
+foreach ($allCurrencies as $currency) {
+    echo $currency["ID"] . " - " . $currency["Rate"] . PHP_EOL;
+}
